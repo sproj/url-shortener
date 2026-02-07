1d1d16571bb3411d667bbb983bcc3cb2ee468439
sproj <jonesalan404@gmail.com>
2026-02-07 11:36:06 +0000

    add health endpoint

--- src/api/server.rs
+++ src/api/server.rs
@@ -1,9 +1,13 @@
 use crate::{api::routes::hello_routes, application::config::Config};
-use axum::{Router, http::StatusCode, response::IntoResponse, extract::Request};
+use axum::{
+    Json, Router, extract::Request, http::StatusCode, response::IntoResponse, routing::get,
+};
+use serde_json::json;
 use tokio::net::TcpListener;
 
 pub async fn start(config: Config) {
     let router = Router::new()
+        .route("/health", get(health_handler))
         .nest("/hello", hello_routes::routes())
         .fallback(error_404_handler);
 
@@ -13,6 +17,13 @@ pub async fn start(config: Config) {
     axum::serve(listener, router).await.unwrap();
 }
 
+// health request handler
+async fn health_handler() -> Result<impl IntoResponse, ()> {
+    Ok(Json(json!({"status": "healthy"})))
+}
+
+// 404 handler
 async fn error_404_handler(request: Request) -> impl IntoResponse {
+    println!("route not found: {:?}", request);
     StatusCode::NOT_FOUND
 }
