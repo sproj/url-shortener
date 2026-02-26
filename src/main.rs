5fa9d293e6cb7b7ca9d08705227901461cd987ca
sproj <jonesalan404@gmail.com>
2026-02-26 13:44:13 +0000

    remove global socket address for tests test_app::run returns a TestApp which has the SocketAddress and a `build_path` replacing helpers::build_path

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use url_shortener::application::{app, startup_error::StartupError};
 
 #[tokio::main]
 async fn main() -> Result<(), StartupError> {
-    let app_state = app::build().await?;
-    app::run(app_state).await?;
+    let config = app::build().await?;
+    app::run(config).await?;
     Ok(())
 }
--- tests/common/test_app.rs
+++ tests/common/test_app.rs
@@ -1,5 +1,5 @@
 use std::time::Duration;
-
+use core::net::SocketAddr;
 use reqwest::StatusCode;
 use tokio::time::Instant;
 use url_shortener::{
@@ -7,9 +7,20 @@ use url_shortener::{
     application::config,
 };
 
-use crate::common::{constants, helpers};
+use crate::common::helpers;
+pub struct TestApp {
+    socket_address: SocketAddr
+}
 
-pub async fn run() {
+impl TestApp {
+    pub fn build_path(&self, path: &str) -> reqwest::Url {
+        let url = format!("http://{}/{}", self.socket_address, path);
+        dbg!("building url: {}", &url);
+        reqwest::Url::parse(&url).unwrap()
+    }
+}
+
+pub async fn run() -> TestApp {
     let config = config::load().unwrap();
     helpers::CONFIG.get_or_init(|| config.clone());
     
@@ -17,26 +28,29 @@ pub async fn run() {
     let addr = listener.local_addr().unwrap();
 
     dbg!(format!("test_app will listen on port: {}", &addr));
-    helpers::ADDR.get_or_init(|| format!("http://{}", addr));
 
     tokio::spawn(server::serve(listener));
     // tokio::spawn(async move { server::serve(listener).await });
 
-    wait_for_service(Duration::from_secs(5)).await
-}
+    // wait_for_service(Duration::from_secs(5)).await
 
-async fn wait_for_service(duration: Duration) {
-    let timeout = Instant::now() + duration;
-    loop {
-        let url = helpers::build_path(constants::API_PATH_HEALTH);
-        if let Ok(response) = reqwest::get(url).await
-            && response.status() == StatusCode::OK
-        {
-            break;
-        }
-        if Instant::now() > timeout {
-            panic!("Could not start API Server in: {:?}", duration);
-        }
-        tokio::time::sleep(Duration::from_millis(20)).await;
+    return TestApp {
+        socket_address: addr
     }
 }
+
+// async fn wait_for_service(duration: Duration) {
+//     let timeout = Instant::now() + duration;
+//     loop {
+//         let url = helpers::build_path(constants::API_PATH_HEALTH);
+//         if let Ok(response) = reqwest::get(url).await
+//             && response.status() == StatusCode::OK
+//         {
+//             break;
+//         }
+//         if Instant::now() > timeout {
+//             panic!("Could not start API Server in: {:?}", duration);
+//         }
+//         tokio::time::sleep(Duration::from_millis(20)).await;
+//     }
+// }
