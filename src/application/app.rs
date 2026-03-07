825151a7bc08d5023b307bc43b91cb7c4319ecbc
sproj <jonesalan404@gmail.com>
2026-03-07 15:18:27 +0000

    add ShortUrlService add ShortUrlService to AppState add CodeGenerator trait add CodeGenerator to ShortUrlService handler no longer passes state down to repository via service in order for repository to have reference to pool - it is initialized with one instead. CodeGenerator on service allows swapping for deterministic generator in test without putting a crowbar into application code for that purpose.

--- src/application/app.rs
+++ src/application/app.rs
@@ -2,11 +2,13 @@ use std::ops::DerefMut;
 use std::sync::Arc;
 
 use crate::application::config;
+use crate::application::repository::short_url_repository::ShortUrlRepository;
+use crate::application::service::short_url::short_url_service::ShortUrlService;
 use crate::application::startup_error::StartupError;
 use crate::application::state::{AppState, SharedState};
 use crate::{api::server, application::config::Config};
 
-use deadpool_postgres::{Config as PgConfig, ManagerConfig, RecyclingMethod};
+use deadpool_postgres::{Config as PgConfig, ManagerConfig, Pool, RecyclingMethod};
 use tokio_postgres::NoTls;
 
 pub async fn load() -> Result<Config, StartupError> {
@@ -16,6 +18,21 @@ pub async fn load() -> Result<Config, StartupError> {
 
 pub async fn build(config: &Config) -> Result<SharedState, StartupError> {
     println!("Creating AppState");
+
+    let pool = create_db_pool(config)?;
+
+    println!("Creating domain services");
+    let short_url_repository = ShortUrlRepository::new(pool.clone());
+    let short_url_service = Arc::new(ShortUrlService::new(short_url_repository));
+
+    Ok(Arc::new(AppState {
+        db_pool: pool,
+        short_url: short_url_service,
+    }))
+}
+
+fn create_db_pool(config: &Config) -> Result<Pool, StartupError> {
+    println!("Creating database connection pool");
     let mut pg = PgConfig::new();
     pg.user = Some(config.db.postgres_user.clone());
     pg.password = Some(config.db.postgres_password.clone());
@@ -31,17 +48,15 @@ pub async fn build(config: &Config) -> Result<SharedState, StartupError> {
         recycling_method: RecyclingMethod::Fast,
     });
 
-    let pool = pg
-        .create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
-        .map_err(StartupError::DbPoolCreation)?;
-
-    Ok(Arc::new(AppState { db_pool: pool }))
+    pg.create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
+        .map_err(StartupError::DbPoolCreation)
 }
 
 mod embedded {
     use refinery::embed_migrations;
     embed_migrations!("src/infrastructure/database/migrations");
 }
+
 pub async fn migrate(state: &SharedState) -> Result<refinery::Report, StartupError> {
     println!("Running migrations");
     let mut conn = state.db_pool.get().await?;
--- src/application/state.rs
+++ src/application/state.rs
@@ -2,7 +2,10 @@ use std::sync::Arc;
 
 use deadpool_postgres::Pool;
 
+use crate::application::service::short_url::short_url_service::ShortUrlService;
+
 pub type SharedState = Arc<AppState>;
 pub struct AppState {
+    pub short_url: Arc<ShortUrlService>,
     pub db_pool: Pool,
 }
