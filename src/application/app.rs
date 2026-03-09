ca482534cf5728d18577a2a21c691138a07ac8dc
sproj <jonesalan404@gmail.com>
2026-03-09 19:01:00 +0000

    move free startup step functions into App/AppBuilder

--- src/application/app.rs
+++ src/application/app.rs
@@ -1,7 +1,6 @@
 use std::ops::DerefMut;
 use std::sync::Arc;
 
-use crate::application::config;
 use crate::application::startup_error::StartupError;
 use crate::application::state::{AppStateBuilder, SharedState};
 use crate::{api::server, application::config::Config};
@@ -33,18 +32,34 @@ impl App {
     }
 
     pub async fn migrate(self) -> Result<Self, StartupError> {
-        let report = crate::application::app::migrate(&self.state).await?;
+        let report = Self::run_migrations(&self.state).await?;
         println!("{:?}", report.applied_migrations());
         Ok(self)
     }
 
     pub async fn start(self) -> Result<(), StartupError> {
         if self.auto_migrate {
-            let report = crate::application::app::migrate(&self.state).await?;
+            let report = Self::run_migrations(&self.state).await?;
             println!("{:?}", report.applied_migrations());
         }
 
-        crate::application::app::run(self.config, self.state).await
+        Self::run_server(self.config, self.state).await
+    }
+
+    async fn run_migrations(state: &SharedState) -> Result<refinery::Report, StartupError> {
+        println!("Running migrations");
+        let mut conn = state.db_pool.get().await?;
+        let client = conn.deref_mut().deref_mut();
+        embedded::migrations::runner()
+            .run_async(client)
+            .await
+            .map_err(StartupError::DbMigrations)
+    }
+
+    async fn run_server(config: Config, state: SharedState) -> Result<(), StartupError> {
+        println!("Starting server");
+        server::start(config, state).await;
+        Ok(())
     }
 }
 
@@ -85,7 +100,7 @@ impl AppBuilder {
     pub async fn build(self) -> Result<App, StartupError> {
         let config = match self.config {
             Some(config) => config,
-            None => load().await?,
+            None => Self::load_config()?,
         };
 
         let state = match self.state {
@@ -93,7 +108,7 @@ impl AppBuilder {
             None => {
                 let db_pool = match self.db_pool {
                     Some(pool) => pool,
-                    None => create_db_pool(&config)?,
+                    None => Self::create_db_pool(&config)?,
                 };
                 Arc::new(self.state_builder.build(db_pool))
             }
@@ -104,6 +119,32 @@ impl AppBuilder {
             state,
             auto_migrate: self.auto_migrate,
         })
+    }
+
+    fn load_config() -> Result<Config, StartupError> {
+        println!("Loading config");
+        crate::application::config::load()
+    }
+
+    fn create_db_pool(config: &Config) -> Result<Pool, StartupError> {
+        println!("Creating database connection pool");
+        let mut pg = PgConfig::new();
+        pg.user = Some(config.db.postgres_user.clone());
+        pg.password = Some(config.db.postgres_password.clone());
+        pg.host = Some(config.db.postgres_host.clone());
+        pg.port = Some(config.db.postgres_port);
+        pg.dbname = Some(config.db.postgres_db.clone());
+        pg.pool = Some(deadpool_postgres::PoolConfig {
+            max_size: config.db.postgres_connection_pool as usize,
+            ..Default::default()
+        });
+
+        pg.manager = Some(ManagerConfig {
+            recycling_method: RecyclingMethod::Fast,
+        });
+
+        pg.create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
+            .map_err(StartupError::DbPoolCreation)
     }
 }
 
@@ -119,65 +160,7 @@ impl Default for AppBuilder {
     }
 }
 
-pub async fn load() -> Result<Config, StartupError> {
-    println!("Loading config");
-    config::load()
-}
-
-pub async fn build(config: &Config) -> Result<SharedState, StartupError> {
-    build_with_state_builder(config, AppStateBuilder::default()).await
-}
-
-pub async fn build_with_state_builder(
-    config: &Config,
-    state_builder: AppStateBuilder,
-) -> Result<SharedState, StartupError> {
-    println!("Creating AppState");
-
-    let pool = create_db_pool(config)?;
-
-    println!("Setting AppState");
-    Ok(Arc::new(state_builder.build(pool)))
-}
-
-fn create_db_pool(config: &Config) -> Result<Pool, StartupError> {
-    println!("Creating database connection pool");
-    let mut pg = PgConfig::new();
-    pg.user = Some(config.db.postgres_user.clone());
-    pg.password = Some(config.db.postgres_password.clone());
-    pg.host = Some(config.db.postgres_host.clone());
-    pg.port = Some(config.db.postgres_port);
-    pg.dbname = Some(config.db.postgres_db.clone());
-    pg.pool = Some(deadpool_postgres::PoolConfig {
-        max_size: config.db.postgres_connection_pool as usize,
-        ..Default::default()
-    });
-
-    pg.manager = Some(ManagerConfig {
-        recycling_method: RecyclingMethod::Fast,
-    });
-
-    pg.create_pool(Some(deadpool_postgres::Runtime::Tokio1), NoTls)
-        .map_err(StartupError::DbPoolCreation)
-}
-
 mod embedded {
     use refinery::embed_migrations;
     embed_migrations!("src/infrastructure/database/migrations");
-}
-
-pub async fn migrate(state: &SharedState) -> Result<refinery::Report, StartupError> {
-    println!("Running migrations");
-    let mut conn = state.db_pool.get().await?;
-    let client = conn.deref_mut().deref_mut();
-    embedded::migrations::runner()
-        .run_async(client)
-        .await
-        .map_err(StartupError::DbMigrations)
-}
-
-pub async fn run(config: config::Config, state: SharedState) -> Result<(), StartupError> {
-    println!("Starting server");
-    server::start(config, state).await;
-    Ok(())
 }
