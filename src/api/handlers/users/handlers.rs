7ae5c4c85610ea043bf3892f852d3ce08c65f7d9
sproj <jonesalan404@gmail.com>
2026-03-20 16:09:40 +0000

    add other `/users` handlers get_all getone by uuid delete oneby uuid update password

--- src/api/handlers/users/handlers.rs
+++ src/api/handlers/users/handlers.rs
@@ -1,17 +1,74 @@
 use axum::{
     Json,
-    extract::{State, rejection::JsonRejection},
+    extract::{Path, State, rejection::JsonRejection},
     http::StatusCode,
 };
+use uuid::Uuid;
 
 use crate::{
     api::{
         error::ApiError,
-        handlers::users::{create_user_request::CreateUserRequest, user_response::UserResponse},
+        handlers::users::{
+            create_user_request::CreateUserRequest, update_password_request::UpdatePasswordRequest,
+            user_response::UserResponse,
+        },
     },
     application::{service::user::create_user_params::CreateUserParams, state::SharedState},
     domain::errors::user_error::UserError,
 };
+
+pub async fn get_all(
+    State(state): State<SharedState>,
+) -> Result<Json<Vec<UserResponse>>, ApiError> {
+    let users = state.users.list_all().await?;
+    Ok(Json(users.into_iter().map(UserResponse::from).collect()))
+}
+
+pub async fn get_one_by_uuid(
+    State(state): State<SharedState>,
+    Path(uuid): Path<Uuid>,
+) -> Result<Json<UserResponse>, ApiError> {
+    tracing::debug!(%uuid, "get user by uuid");
+    match state.users.get_one_by_uuid(uuid).await? {
+        Some(user) => Ok(Json(user.into())),
+        None => {
+            tracing::warn!(%uuid, "user not found");
+            Err(ApiError::from(UserError::NotFound(uuid.to_string())))
+        }
+    }
+}
+
+pub async fn delete_one_by_uuid(
+    State(state): State<SharedState>,
+    Path(uuid): Path<Uuid>,
+) -> Result<Json<String>, ApiError> {
+    if state.users.delete_one_by_uuid(uuid).await? {
+        tracing::debug!(%uuid, "user deleted");
+        Ok(Json(uuid.to_string()))
+    } else {
+        tracing::warn!(%uuid, "user not found for delete");
+        Err(ApiError::from(UserError::NotFound(uuid.to_string())))
+    }
+}
+
+pub async fn update_password(
+    State(state): State<SharedState>,
+    Path(uuid): Path<Uuid>,
+    req_payload: Result<Json<UpdatePasswordRequest>, JsonRejection>,
+) -> Result<StatusCode, ApiError> {
+    let Json(parsed_input) =
+        req_payload.map_err(|e| UserError::UnprocessableInput(e.to_string()))?;
+
+    if state
+        .users
+        .update_password_by_uuid(parsed_input.password, uuid)
+        .await?
+    {
+        Ok(StatusCode::OK)
+    } else {
+        Err(ApiError::from(UserError::NotFound(uuid.to_string())))
+    }
+}
 
 pub async fn create_user(
     State(state): State<SharedState>,
