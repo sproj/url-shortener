8e2e14d65cf122da78d2be5ffc3e50ded55eed03
sproj <jonesalan404@gmail.com>
2026-04-06 13:28:53 +0100

    add in memory mock users repository add some unit tests of user service using this mock

--- src/application/service/user/user_service.rs
+++ src/application/service/user/user_service.rs
@@ -98,3 +98,87 @@ impl UserServiceTrait for UsersService {
             .map_err(UserError::Storage)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::{errors::RepositoryError, traits::InMemoryMockUsersRepository};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn list_all_returns_empty_correctly() {
+        let sut = UsersService::new(Arc::new(InMemoryMockUsersRepository::new(vec![])));
+        let actual = sut.list_all().await.unwrap();
+        assert!(actual.is_empty());
+    }
+
+    #[tokio::test]
+    async fn add_one_succeeds() {
+        let sut = UsersService::new(Arc::new(InMemoryMockUsersRepository::new(vec![])));
+        let params = CreateUserParams {
+            username: "add_one_succeeds".to_string(),
+            email: "add_one@succeeds".to_string(),
+            password: "does not matter".to_string(),
+            roles: "user".to_string(),
+            active: true,
+        };
+
+        let actual = sut.add_user(params.clone()).await.unwrap();
+        assert_eq!(&actual.username, &params.username);
+        assert_eq!(&actual.email, &params.email);
+        assert!(!&actual.uuid.is_nil())
+    }
+
+    #[tokio::test]
+    async fn add_one_fails_on_duplicate_username() {
+        let sut = UsersService::new(Arc::new(InMemoryMockUsersRepository::new(vec![])));
+        let params = CreateUserParams {
+            username: "add_one_fails_on_duplicate_username".to_string(),
+            email: "add_one_fails@on_duplicate_username".to_string(),
+            password: "does not matter".to_string(),
+            roles: "user".to_string(),
+            active: true,
+        };
+
+        let add_user = sut.add_user(params.clone()).await.unwrap();
+        assert_eq!(&add_user.username, &params.username);
+        assert_eq!(&add_user.email, &params.email);
+        assert!(!&add_user.uuid.is_nil());
+
+        let actual = sut.add_user(params.clone()).await;
+
+        assert!(actual.is_err());
+
+        let err = actual.unwrap_err();
+
+        assert!(matches!(
+            err,
+            UserError::Storage(RepositoryError::Conflict { .. })
+        ))
+    }
+
+    #[tokio::test]
+    async fn delete_one_succeeds() {
+        let sut = UsersService::new(Arc::new(InMemoryMockUsersRepository::new(vec![])));
+        let params = CreateUserParams {
+            username: "add_one_succeeds".to_string(),
+            email: "add_one@succeeds".to_string(),
+            password: "does not matter".to_string(),
+            roles: "user".to_string(),
+            active: true,
+        };
+
+        let user = sut.add_user(params.clone()).await.unwrap();
+
+        assert!(user.deleted_at.is_none());
+
+        let user_uuid = user.uuid;
+
+        let actual = sut.delete_one_by_uuid(user.uuid).await.unwrap();
+
+        assert!(actual);
+
+        let check = sut.get_one_by_uuid(user_uuid).await.unwrap();
+        assert!(check.is_some_and(|usr| usr.deleted_at.is_some()));
+    }
+}
--- src/domain/traits/mod.rs
+++ src/domain/traits/mod.rs
@@ -2,4 +2,5 @@ mod short_url_repository_trait;
 mod users_repository_trait;
 
 pub use short_url_repository_trait::ShortUrlRepositoryTrait;
+pub use users_repository_trait::InMemoryMockUsersRepository;
 pub use users_repository_trait::UsersRepositoryTrait;
