27a49deb89f0d02f6d10a338954ca162bc658edb
sproj <jonesalan404@gmail.com>
2026-04-29 16:53:05 +0100

    emit redirect event on redirect

--- src/api/handlers/redirect.rs
+++ src/api/handlers/redirect.rs
@@ -1,13 +1,22 @@
+use std::sync::Arc;
+
 use axum::{
     extract::{Path, State},
     http::{HeaderValue, Method, StatusCode, header},
     response::{IntoResponse, Response},
 };
+use chrono::Utc;
 use tracing::instrument;
 
 use crate::{
     api::error::ApiError,
-    application::{service::short_url::short_url_service::RedirectDecision, state::SharedState},
+    application::{
+        service::{
+            analytics::analytics_publisher_trait::{RedirectEvent, RedirectType},
+            short_url::short_url_service::RedirectDecision,
+        },
+        state::SharedState,
+    },
     domain::errors::ShortUrlError,
 };
 
@@ -39,15 +48,35 @@ pub async fn redirect(
     match decision {
         RedirectDecision::Gone => Ok(StatusCode::GONE.into_response()),
         RedirectDecision::NotFound => Err(ApiError::from(ShortUrlError::NotFound(code))),
-        RedirectDecision::Permanent { long_url } => match method {
-            Method::GET => redirect_response(StatusCode::MOVED_PERMANENTLY, &long_url),
-            _ => redirect_response(StatusCode::PERMANENT_REDIRECT, &long_url),
-        },
-        RedirectDecision::Temporary { long_url } => match method {
-            Method::GET => redirect_response(StatusCode::FOUND, &long_url),
-            _ => redirect_response(StatusCode::TEMPORARY_REDIRECT, &long_url),
-        },
+        RedirectDecision::Permanent { long_url } => {
+            emit_analytics(&state, code.clone(), RedirectType::Permanent);
+            match method {
+                Method::GET => redirect_response(StatusCode::MOVED_PERMANENTLY, &long_url),
+                _ => redirect_response(StatusCode::PERMANENT_REDIRECT, &long_url),
+            }
+        }
+        RedirectDecision::Temporary { long_url } => {
+            emit_analytics(&state, code.clone(), RedirectType::Temporary);
+            match method {
+                Method::GET => redirect_response(StatusCode::FOUND, &long_url),
+                _ => redirect_response(StatusCode::TEMPORARY_REDIRECT, &long_url),
+            }
+        }
     }
+}
+
+fn emit_analytics(state: &SharedState, code: String, redirect_type: RedirectType) {
+    let publisher = Arc::clone(&state.analytics_publisher);
+    let event = RedirectEvent {
+        code,
+        timestamp: Utc::now(),
+        redirect_type,
+    };
+    tokio::spawn(async move {
+        if let Err(e) = publisher.publish(event).await {
+            tracing::warn!(%e, "analytics publish failed");
+        }
+    });
 }
 
 fn redirect_response(status: StatusCode, location: &str) -> Result<Response, ApiError> {
