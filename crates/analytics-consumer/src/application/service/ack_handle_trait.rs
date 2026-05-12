4423d06ecbeb7a4ea96c0a683efce3c64edcf27d
sproj <jonesalan404@gmail.com>
2026-05-12 20:46:36 +0100

    add nack to AckHandle

--- crates/analytics-consumer/src/application/service/ack_handle_trait.rs
+++ crates/analytics-consumer/src/application/service/ack_handle_trait.rs
@@ -3,4 +3,5 @@ use crate::infrastructure::messaging::messaging_error::MessagingError;
 #[async_trait::async_trait]
 pub trait AckHandle: Send {
     async fn ack(self: Box<Self>) -> Result<(), MessagingError>;
+    async fn nack(self: Box<Self>, requeue: bool) -> Result<(), MessagingError>;
 }
