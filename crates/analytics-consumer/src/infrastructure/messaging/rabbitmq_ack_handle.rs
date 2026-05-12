4fd86229a838af0bed83a5c5709483efa8a8c66c
sproj <jonesalan404@gmail.com>
2026-05-12 20:46:36 +0100

    add nack to AckHandle

--- crates/analytics-consumer/src/infrastructure/messaging/rabbitmq_ack_handle.rs
+++ crates/analytics-consumer/src/infrastructure/messaging/rabbitmq_ack_handle.rs
@@ -1,4 +1,4 @@
-use lapin::options::BasicAckOptions;
+use lapin::options::{BasicAckOptions, BasicNackOptions};
 
 use crate::{
     application::service::ack_handle_trait::AckHandle,
@@ -13,6 +13,15 @@ pub struct RabbitMqAckHandle {
 impl AckHandle for RabbitMqAckHandle {
     async fn ack(self: Box<Self>) -> Result<(), MessagingError> {
         self.delivery.ack(BasicAckOptions::default()).await?;
+        Ok(())
+    }
+    async fn nack(self: Box<Self>, requeue: bool) -> Result<(), MessagingError> {
+        self.delivery
+            .nack(BasicNackOptions {
+                requeue,
+                ..Default::default()
+            })
+            .await?;
         Ok(())
     }
 }
